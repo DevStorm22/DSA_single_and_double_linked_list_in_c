935f17959cd7e81ab3fce10e20979e21af3d9c43
Kaushik <kaushikboss0@gmail.com>
2025-11-18 10:06:41 +0530

    Singlely linked list

--- operation_on_linked _list_function.c
+++ operation_on_linked _list_function.c
@@ -64,10 +64,10 @@ int main() {
   int option, c, temp, position; // Declaring variables
 
   // Creation and initialization of Linked list
-  h = creation_and_initialization();
+h = creation_and_initialization();
 
   // Menu
-  do {
+do {
     printf(
         "\nOperation of the linked list\n1.Counting the nodes of the linked "
         "list\n2.Searching the element form the linked list\n3.Insertion of "
@@ -145,213 +145,243 @@ int main() {
 }
 // Function definition
 node *creation_and_initialization() {
-  node *head, *p;
-  int size, i;
-  head = (node *)malloc(sizeof(node));
-  // Taking the element from the user
-  printf("Enter the size of the linked list: ");
-  scanf("%d", &size);
-  printf("Enter the head: ");
-  scanf("%d", &(head->data));
-  head->next = NULL;
-  // Taking the element from the user
-  p = head;
-  for (i = 1; i < size; i++) {
-    p->next = (node *)malloc(sizeof(node));
-    p = p->next;
-    p->next = NULL;
-    printf("Enter the element %d: ", i + 1);
-    scanf("%d", &(p->data));
-  }
-  return head;
+    node *head, *p;
+    int size, i;
+    head = (node *)malloc(sizeof(node));
+    // Taking the element from the user
+    printf("Enter the size of the linked list: ");
+    scanf("%d", &size);
+    printf("Enter the head: ");
+    scanf("%d", &(head->data));
+    head->next = NULL;
+    // Taking the element from the user
+    p = head;
+    for (i = 1; i < size; i++)
+    {
+            p->next = (node *)malloc(sizeof(node));
+            p = p->next;
+            p->next = NULL;
+            printf("Enter the element %d: ", i + 1);
+            scanf("%d", &(p->data));
+    }
+    return head;
 }
 
 int counting_nodes(node *head) {
-  node *p;
-  int c;
-  p = (node *)malloc(sizeof(node));
-  p = head;
-  c = 0;
-  while (p != NULL) {
-    c++;
-    p = p->next;
-  }
-  return c;
+    node *p;
+    int c;
+    p = (node *)malloc(sizeof(node));
+    p = head;
+    c = 0;
+    while (p != NULL)
+    {
+        c++;
+        p = p->next;
+    }
+    return c;
 }
 
 node *searching_element(node *head, int temp) {
-  node *p;
-  int flag = 0;
-  p = (node *)malloc(sizeof(node));
-  p = head;
-  while (p != NULL) {
-    if (temp == p->data) {
-      flag = 1;
-      break;
+    node *p;
+    int flag = 0;
+    p = (node *)malloc(sizeof(node));
+    p = head;
+    while (p != NULL)
+    {
+        if (temp == p->data)
+        {
+            flag = 1;
+            break;
+        }
+        p = p->next;
     }
-    p = p->next;
-  }
-  if (flag) {
-    printf("Element founded");
-  } else {
-    printf("Element not founded");
-  }
-  return head;
+    if (flag)
+    {
+        printf("Element founded");
+    }
+    else
+    {
+        printf("Element not founded");
+    }
+    return head;
 }
 
-node *insertion_of_node_at_the_beginning(node *head) {
-  node *p, *q;
-  p = (node *)malloc(sizeof(node));
-  q = (node *)malloc(sizeof(node));
-  q = head;
-  printf("Enter data for the new node (before first node): ");
-  scanf("%d", &(p->data));
-  p->next = q;
-  return p;
+node *insertion_of_node_at_the_beginning(node *head)
+{
+    node *p, *q;
+    p = (node *)malloc(sizeof(node));
+    q = (node *)malloc(sizeof(node));
+    q = head;
+    printf("Enter data for the new node (before first node): ");
+    scanf("%d", &(p->data));
+    p->next = q;
+    return p;
 }
 
-node *insertion_of_node_at_the_given_position(node *head, int position) {
-  node *p, *q;
-  int c;
-  p = head;
-  c = 0;
-  while (c != position - 2) {
-    c++;
-    p = p->next;
-  }
-  q = (node *)malloc(sizeof(node));
-  printf("Enter data for the new node: ");
-  scanf("%d", &(q->data));
-  q->next = p->next;
-  p->next = q;
-  return head;
+node *insertion_of_node_at_the_given_position(node *head, int position)
+{
+    node *p, *q;
+    int c;
+    p = head;
+    c = 0;
+    while (c != position - 2)
+    {
+        c++;
+        p = p->next;
+    }
+    q = (node *)malloc(sizeof(node));
+    printf("Enter data for the new node: ");
+    scanf("%d", &(q->data));
+    q->next = p->next;
+    p->next = q;
+    return head;
 }
 
-node *insertion_of_node_at_the_end(node *head) {
-  node *p, *q;
-  p = head;
-  while (p->next != NULL) {
-    p = p->next;
-  }
-  q = (node *)malloc(sizeof(node));
-  p->next = q;
-  q->next = NULL;
-  printf("Enter data for the new node (after last node): ");
-  scanf("%d", &(q->data));
-  return head;
+node *insertion_of_node_at_the_end(node *head)
+{
+    node *p, *q;
+    p = head;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    q = (node *)malloc(sizeof(node));
+    p->next = q;
+    q->next = NULL;
+    printf("Enter data for the new node (after last node): ");
+    scanf("%d", &(q->data));
+    return head;
 }
 
-node *deletion_of_node_at_beginning(node *head) {
-  node *p, *q;
-  q = head;
-  head = head->next;
-  free(q);
-  printf("\nNode remove");
-  return head;
+node *deletion_of_node_at_beginning(node *head)
+{
+    node *p, *q;
+    q = head;
+    head = head->next;
+    free(q);
+    printf("\nNode remove");
+    return head;
 }
 
-node *deletion_of_node_at_given_position(node *head, int position) {
-  node *p, *q, *t;
-  int c = 0;
-  p = head;
-  while (c != position - 2) {
-    c++;
-    p = p->next;
-  }
-  printf("\nNode remove");
-  q = p->next;
-  t = q->next;
-  p->next = t;
-  free(q);
-  return head;
-}
-
-node *deletion_of_node_at_end(node *head) {
-  node *p, *q;
-  p = head;
-  while (p->next->next != NULL) {
-    p = p->next;
-  }
-  q = p->next;
-  p->next = NULL;
-  free(q);
-  printf("\nNode remove");
-  return head;
-}
-
-node *reversing_the_linked_list(node *head) {
-  node *p, *q, *t;
-  p = t = head;
-  q = NULL;
-  while (p != NULL) {
-    p = p->next;
-    t->next = q;
-    q = t;
-    t = p;
-  }
-  return q;
-}
-
-node *sorting_the_linked_list(node *head) {
-  node *p, *q;
-  int temp;
-  for (p = head; p->next != NULL; p = p->next) {
+node *deletion_of_node_at_given_position(node *head, int position)
+{
+    node *p, *q, *t;
+    int c = 0;
+    p = head;
+    while (c != position - 2)
+    {
+        c++;
+        p = p->next;
+    }
+    printf("\nNode remove");
     q = p->next;
-    for (q = p->next; q != NULL; q = q->next) {
-      if (p->data > q->data) {
-        temp = p->data;
-        p->data = q->data;
-        q->data = temp;
-      }
+    t = q->next;
+    p->next = t;
+    free(q);
+    return head;
+}
+
+node *deletion_of_node_at_end(node *head)
+{
+    node *p, *q;
+    p = head;
+    while (p->next->next != NULL)
+    {
+        p = p->next;
     }
-  }
-  return head;
+    q = p->next;
+    p->next = NULL;
+    free(q);
+    printf("\nNode remove");
+    return head;
 }
 
-node *concatinating_the_linked_list(node *head1, node *head2) {
-  node *p, *h;
-  int s, i;
-  p = head1;
-  while (p->next != NULL) {
-    p = p->next;
-  }
-  p->next = head2;
-  return head1;
+node *reversing_the_linked_list(node *head)
+{
+    node *p, *q, *t;
+    p = t = head;
+    q = NULL;
+    while (p != NULL)
+    {
+        p = p->next;
+        t->next = q;
+        q = t;
+        t = p;
+    }
+    return q;
 }
 
-node *separating_the_linked_list(node *head, int position) {
-  node *p, *h;
-  int c = 0;
-  p = head;
-  while (c != position - 2) {
-    c++;
-    p = p->next;
-  }
-  h = p->next;
-  p->next = NULL;
-  p = head;
-  display_linked_list(p);
-  printf("\n");
-  p = h;
-  display_linked_list(p);
-  printf("\n");
-  return head;
+node *sorting_the_linked_list(node *head)
+{
+    node *p, *q;
+    int temp;
+    for (p = head; p->next != NULL; p = p->next)
+    {
+        q = p->next;
+        for (q = p->next; q != NULL; q = q->next)
+        {
+            if (p->data > q->data)
+            {
+                temp = p->data;
+                p->data = q->data;
+                q->data = temp;
+            }
+        }
+    }
+    return head;
 }
 
-node *merging_the_sorted_linked_list(node *head) {
-  node *p, *h, *q;
-  q = creation_and_initialization();
-  p = sorting_the_linked_list(head);
-  q = sorting_the_linked_list(q);
-  h = concatinating_the_linked_list(p, q);
-  return h;
+node *concatinating_the_linked_list(node *head1, node *head2)
+{
+    node *p, *h;
+    int s, i;
+    p = head1;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = head2;
+    return head1;
 }
 
-void display_linked_list(node *head) {
-  node *p;
-  p = head;
-  while (p != NULL) {
-    printf("%d ->", (p->data));
-    p = p->next;
-  }
+node *separating_the_linked_list(node *head, int position)
+{
+    node *p, *h;
+    int c = 0;
+    p = head;
+    while (c != position - 2)
+    {
+        c++;
+        p = p->next;
+    }
+    h = p->next;
+    p->next = NULL;
+    p = head;
+    display_linked_list(p);
+    printf("\n");
+    p = h;
+    display_linked_list(p);
+    printf("\n");
+    return head;
+}
+
+node *merging_the_sorted_linked_list(node *head)
+{
+    node *p, *h, *q;
+    q = creation_and_initialization();
+    p = sorting_the_linked_list(head);
+    q = sorting_the_linked_list(q);
+    h = concatinating_the_linked_list(p, q);
+    return h;
+}
+
+void display_linked_list(node *head)
+{
+    node *p;
+    p = head;
+    while (p != NULL)
+    {
+        printf("%d ->", (p->data));
+        p = p->next;
+    }
 }
